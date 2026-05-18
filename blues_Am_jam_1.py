bd3ad45c0697e3523842d0bac6189ab5e90a1603
Jordi Aubert <jordiaubert01@gmail.com>
2026-05-18 22:43:13 +0200

    updated blues in A minor progression

--- blues_Am_jam_1.py
+++ blues_Am_jam_1.py
@@ -1,16 +1,16 @@
 import PyJammer as bb
 
 intro = "_"
-progression = "Am7|Am7|Dm7|Dm7|Am7|Am7|E7|_"
+progression = "Am7|Am7|Am7|Am7|Dm7|Dm7|Am7|Am7|Em7|Dm7|Am7|E7."
 ending = "_|Am7"
 
 jammer = bb.PyJammer()
-jammer.set_bpm(80)
+jammer.set_bpm(115)
 
-jammer.play_progression(intro, pattern="swing", instrument='piano', bass_line='none', arpeggio=True, silence_drums=False)
+jammer.play_progression(intro, pattern="", instrument='piano', bass_line='none', arpeggio=True, silence_drums=False)
 
 for i in range(20):
-    jammer.play_progression(progression, pattern="swing", instrument='piano', bass_line='simple', arpeggio=False)
+    jammer.play_progression(progression, pattern="swing", instrument='piano', bass_line='blues', arpeggio=False)
 
 jammer.play_progression(ending, pattern="swing", instrument='piano', bass_line='none', arpeggio=True, silence_drums=True)
 
