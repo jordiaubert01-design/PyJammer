b8a3bf84683e839df8482d81d65eb71786f1e395
Jordi Aubert <jordiaubert01@gmail.com>
2026-05-18 22:36:46 +0200

    new sample file: blues in A minor

---
+++ blues_Am_jam_1.py
@@ -0,0 +1,17 @@
+import PyJammer as bb
+
+intro = "_"
+progression = "Am7|Am7|Dm7|Dm7|Am7|Am7|E7|_"
+ending = "_|Am7"
+
+jammer = bb.PyJammer()
+jammer.set_bpm(80)
+
+jammer.play_progression(intro, pattern="swing", instrument='piano', bass_line='none', arpeggio=True, silence_drums=False)
+
+for i in range(20):
+    jammer.play_progression(progression, pattern="swing", instrument='piano', bass_line='simple', arpeggio=False)
+
+jammer.play_progression(ending, pattern="swing", instrument='piano', bass_line='none', arpeggio=True, silence_drums=True)
+
+jammer.close()
