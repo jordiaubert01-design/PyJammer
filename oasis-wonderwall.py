657f04de3b1c52b73ef8e4ac13d5b9dee63a9bf7
Jordi Aubert <jordiaubert01@gmail.com>
2026-09-13 12:26:15 +0200

    wonderwall improved

--- oasis-wonderwall.py
+++ oasis-wonderwall.py
@@ -1,12 +1,16 @@
 import PyJammer as bb
 
 intro = "_"
-progr1 = "Em|G|D|A7"
+progr1 = "Em7,G|D,A7"
+prog2 = "C,Em7|G,Em7"
 
 jammer = bb.PyJammer()
-jammer.set_bpm(110)
+jammer.set_bpm(87)
 
-jammer.play_progression(intro, pattern="hihat", instrument='none', bass_line='simple', arpeggio=False)
-jammer.play_progression(progr1, pattern="hihat", instrument='none', bass_line='simple', arpeggio=False)
+jammer.play_progression(intro, pattern="hihat", instrument='none', bass_line='none')
+jammer.play_progression(progr1, pattern="standard", instrument='none', bass_line='pop', repetitions=4, text="(intro)")
+jammer.play_progression(progr1, pattern="standard", instrument='none', bass_line='pop', repetitions=4, text="(verse1)")
+jammer.play_progression(progr1, pattern="standard", instrument='none', bass_line='pop', repetitions=4, text="(verse2)")
+jammer.play_progression(prog2, pattern="standard", instrument='none', bass_line='simple', repetitions=4, text="(chorus)")
     
 jammer.close()
